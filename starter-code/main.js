021027ce22a5efa4a13e1048b33336599ccfb8db
jm <jungmin.yjm@gmail.com>
2016-10-29 00:14:02 -0400

    unit 10

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,19 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+//if (cardTwo === cardFour) {
+//	alert ('You found a match!');
+//} else if (cardTwo !== cardFour) {
+//	alert ('Sorry, try again');
+//}
+
+var board = document.getElementById(game-board);
+
+function createCards() {
+	for (i=0; card.length; i++) {
+		var gamecard = document.createElement('div');
+		gamecard.className = 'card';
+		board.appendChild(gamecard)
+	}
+}
